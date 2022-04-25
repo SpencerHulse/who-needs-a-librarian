460a4e0410eff60a3d628727fa4aabea2988ed14
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-25 18:16:29 -0400

    Added typeDefs, including query and mutation definitions.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,52 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(email: String!, password: String!, username: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
+  }
+`;
 
 module.exports = typeDefs;
+
+// bookId is the ID returned from Google's Book API
+// authors is an array of strings
+
+// Used an input type for saveBook---New - Potential problem spot.
